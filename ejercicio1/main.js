8647f2efad1da941cbca4ead99dc060d9abaed26
Arteconjota <javier.piserrano@gmail.com>
2025-03-21 18:34:28 +0100

    subida

---
+++ ejercicio1/main.js
@@ -0,0 +1,25 @@
+//CONECTO LA API
+fetch("https://thronesapi.com/api/v2/Characters")
+    .then(response => response.json())
+    .then(data => {
+        const characterList = document.querySelector("#character-list");
+
+        data.forEach(character => {
+            let opt = document.createElement("option");
+            opt.value = character.imageUrl;
+            opt.textContent = character.fullName;
+            characterList.appendChild(opt);
+        });
+
+        updateCharacterImage(characterList.value);
+    })
+    .catch(error => console.error("Error al cargar los personajes:", error));
+
+function updateCharacterImage(imageUrl) {
+    const imgElement = document.querySelector(".character-image");
+    imgElement.src = imageUrl;
+}
+
+document.querySelector("#character-list").addEventListener("change", function () {
+    updateCharacterImage(this.value);
+});
